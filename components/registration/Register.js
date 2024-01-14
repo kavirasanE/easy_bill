e574e8a16b4dc287e2ab644ee8b2dca9a1d5658c
einsteinkavi98@gmail.com <kavirasan1998@gmail.com>
2024-01-15 03:50:19 +0530

    final

--- components/registration/Register.js
+++ components/registration/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, Text, TextInput } from "react-native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import Checkbox from "expo-checkbox";
 import { Picker } from "@react-native-picker/picker";
 
@@ -8,7 +8,7 @@ const Register = () => {
     yes: false,
     No: false,
   };
-  const [selectedLanguage, setSelectedLanguage] = useState('java');
+  const [selectedValue, setSelectedValue] = useState('java');
   const [checked, setChecked] = useState(initialState);
 
   return (
@@ -17,31 +17,42 @@ const Register = () => {
         <Text className=" font-bold text-2xl text-sky-700 "> Create User</Text>
         <Text className="ml-2">Please enter your details</Text>
       </View>
-      <View className="m-2 mt-4">
+      <View className="m-2 mt-4 ">
         <TextInput
           placeholder="Shop Name"
-          className="border-2 border-sky-700 p-2 mb-6 "
+          className="border-2 border-sky-700 p-2 mb-4 "
         />
         <TextInput
           placeholder="Address 1"
-          className="border-2 border-sky-700 p-2 mb-6"
+          className="border-2 border-sky-700 p-2 mb-4"
         />
         <TextInput
           placeholder="Address 2"
-          className="border-2 border-sky-700 p-2 mb-6"
+          className="border-2 border-sky-700 p-2 mb-4"
         />
         <TextInput
           placeholder="City"
-          className="border-2 border-sky-700 p-2 mb-6"
+          className="border-2 border-sky-700 p-2 mb-4"
         />
         <TextInput
           placeholder="Email Address"
-          className="border-2 border-sky-700 p-2 mb-6"
+          className="border-2 border-sky-700 p-2 mb-4"
         />
         <TextInput
           placeholder="Mobile Number"
-          className="border-2 border-sky-700 p-2 mb-2"
+          className="border-2 border-sky-700 p-2 mb-4"
         />
+         <View className="border-2 border-sky-700 text-black h-12 justify-center ">
+      <Picker
+        selectedValue={selectedValue}
+        onValueChange={(itemValue) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Java" value="java" />
+        <Picker.Item label="JavaScript" value="js" />
+        <Picker.Item label="Python" value="python" />
+        <Picker.Item label="C#" value="csharp" />
+      </Picker>
+        </View>
         <Text className="text-md font-bold">GST Applicable:</Text>
         <View className="flex flex-row gap-4 justify-start mx-2 ">
           <View className="flex flex-row gap-2">
@@ -67,19 +78,14 @@ const Register = () => {
           placeholder="GST Number"
           className="border-2 border-sky-700 p-2 mt-6 mb-4"
         />
+        
        
       </View>
-      <View className="border-2 border-sky-700 text-black ">
-          <Picker
-            selectedValue={selectedLanguage}
-            onValueChange={(itemValue) =>
-              setSelectedLanguage(itemValue)
-            }
-            >
-            <Picker.Item label="Java" value="java" />
-            <Picker.Item label="JavaScript" value="js" />
-          </Picker>
-        </View>
+      <View className="mx-8 rou">
+        <TouchableOpacity onPress="function" className="border-2 border-black p-3 justify-center items-center my-2 bg-sky-700">
+          <Text className="text-white font-bold">Submit</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
